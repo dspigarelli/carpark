1e661a7ee2d43a2534fa33edfb9f51d212b8dd01
itspomf <jxpetrillo@gmail.com>
2019-10-09 19:40:30 -0400

    Refactoring
    * Change enforcement pattern to be vastly cleaner and more logical.
    
    * Update MongoDB code to actually allow multiple records so we have a 
    self-generating history (uses ObjectID) -- the "client" is responsible 
    for tracking this value for handling departures.
    
    * Upserts are no longer used, which should have better performance and 
    help with the history feature.
    
    * General bug fixes to add missing 'async' keyword to inner scopes for 
    multipartite operations.

--- carpark.js
+++ carpark.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 const { createServer } = require('http');
 const { readFile } = require('fs');
 
@@ -23,22 +23,26 @@ async function query( op ){
   return results;
 }
 
-// Register a new vehicle, by its licence plate.  This tracks when the vehicle entered the system.
+// Register a new vehicle, by its licence plate.  This tracks when the vehicle entered the system.  Returns the Mongo
+// ObjectID for the newly created record; this is used for handling outbound updates.
 async function inbound( license, arrival = new Date() ){
-  return query((, cars ) => cars.updateOne({ license }, { $set: { arrival, departure: null }}, { upsert: true }));
+  return query( async (, cars ) => {
+    const record = await cars.insert({ license, arrival, departure: null });
+    return record.insertedId;
+  });
 }
 
 // Update the departure of a vehicle.  Returns the total time, in seconds, that it was parked in the garage.
-async function outbound( license, departure = new Date() ){
-  return query((, cars ) => {
-    const record = await cars.findOneAndUpdate({ license }, { $set: { departure }});
+async function outbound( id, departure = new Date() ){
+  return query( async (, cars ) => {
+    const record = await cars.findOneAndUpdate({ _id: new ObjectID( id ) }, { $set: { departure }});
     return ( departure.getTime() - record.arrival.getTime() ) * 1000;
   });
 }
 
-// Check for vehicle presence.  Returns the record(s).
+// Check for vehicle presence.  Returns the record(s) found, if any.
 async function find( license, limit = 1 ){
-  return query((, cars ) => cars.find({ license }).limit( limit ).toArray() );
+  return query((, cars ) => cars.find({ license, departure: null }).limit( limit ).toArray() );
 }
 
 // Compute the charge for parking.
@@ -46,30 +50,26 @@ function computeCharge( seconds ){
   return ( seconds / 3600 ) * ( process.env.rate || 7.50 );
 }
 
-// Enforce a REST method, otherwise error.
-function enforce( req, res, assert ){
-  if( req.method === assert )
-    return true;
-
-  // failure
+// Respond with a Not Allowed.
+function notAllowed( res ){
   res.writeHead( 405, { 'Content-Type': 'application/json' });
   res.end({
     message: 'Method Not Allowed',
     expected: assert,
     received: req.method
   });
-  return false;
 }
 
 // Respond to API requests.
 function respond( res, data ){
   res.writeHead( 200, { 'Content-Type': 'application/json' });
-  res.end( data );
+  res.end({ message: 'OK', data });
 }
 
 // should update this to use SSL/TLS, but don't have a means to create certs right now
 const address = process.env.hostname || '0.0.0.0';
 createServer( async( req, res ) => {
+
   // default web server logic -- get our static file target
   if( !/^\/api\/i.test( req.path )){
     const target = !/\.[^\/]+$/.test( req.path ) ? `${req.path}index.html` : req.path;
@@ -101,27 +101,29 @@ createServer( async( req, res ) => {
 
         // a new vehicle has arrived
         case 'inbound':
-          if( enforce( req, res, 'POST'))
-            return respond( res, await inbound( vehicle.license, vehicle.arrival || new Date() ));
+          if( req.method !== 'POST')
+            return notAllowed( res );
+          return respond( res, await inbound( vehicle.license, vehicle.arrival || new Date() ));
 
         // a vehicle has left, get the fee
         case 'outbound':
-          if( enforce( req, res, 'POST')){
-            const time = await outbound( vehicle.license );
-            return respond( res, { fee: computeCharge( time ) });
-          }
+          if( req.method !== 'POST')
+            return notAllowed( res );
+          const time = await outbound( vehicle.recordID );
+          return respond( res, { fee: computeCharge( time ) });
 
         // we need to determine the charge for how long they parked
         case 'fee':
-          if( enforce( req, res, 'GET'))
-            return respond( res, computeCharge( vehicle.timeParked ));
+          if( req.method !== 'GET')
+            return notAllowed( res );
+          return respond( res, computeCharge( vehicle.timeParked ));
 
         // we want to know if someone is still parked here
         case 'parked':
-          if( enforce( req, res, 'GET')){
-            const hits = await find( vehicle.license );
-            return respond( res, { parked: hits.length >= 1 });
-          }
+          if( req.method !== 'GET')
+            return notAllowed( res );
+          const hits = await find( vehicle.license );
+          return respond( res, { parked: hits.length >= 1 });
       }
     });
   } catch( err ){
